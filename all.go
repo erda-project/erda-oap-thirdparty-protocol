ccf414b99ea066322cf58d828000847130d62881
liuhaoyang <liuhaoyang1221@hotmail.com>
2021-09-06 16:43:29 +0800

    Fix agent import

--- all.go
+++
@@ -1,9 +0,0 @@
-package oap_plugins_gen_go
-
-import (
-	_ "github.com/erda-project/oap-plugins-gen-go/jaeger-thrift/agent"
-	_ "github.com/erda-project/oap-plugins-gen-go/jaeger-thrift/baggage"
-	_ "github.com/erda-project/oap-plugins-gen-go/jaeger-thrift/jaeger"
-	_ "github.com/erda-project/oap-plugins-gen-go/jaeger-thrift/sampling"
-	_ "github.com/erda-project/oap-plugins-gen-go/jaeger-thrift/zipkincore"
-)
